ec2619a0a92f8c3ba45817d95396fbbac9182fcd
Marcos Kohler <marcosk@edu.univali.br>
2024-08-30 10:54:29 -0300

    Ajuste no calculo
    Feito ajuste  para puxar tag correta da xml

--- js/xmlParser.js
+++ js/xmlParser.js
@@ -28,7 +28,7 @@ function parseProducts(xmlDoc) {
       name: productNode.querySelector('xProd')?.textContent || 'Sem nome',
       code: productNode.querySelector('cProd')?.textContent || '0',
       price: parseFloat(productNode.querySelector('vUnCom')?.textContent || 0),
-      quantity: parseFloat(productNode.querySelector('qCom')?.textContent || 0),
+      quantity: parseFloat(productNode.querySelector('qTrib')?.textContent || 0),
       value: parseFloat(productNode.querySelector('vProd')?.textContent || 0),
       ipi: parseFloat(productNode.querySelector('vIPI')?.textContent || 0),
       icmsST: parseFloat(productNode.querySelector('vICMSST')?.textContent || 0),
